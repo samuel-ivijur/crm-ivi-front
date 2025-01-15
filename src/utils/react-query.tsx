7edf6485eb8617fd017bc2a2f1a056e0c2f44efb
Samuel Ivi <samuel@ivijur.com.br>
2025-01-15 09:49:51 -0300

    Fix some conflicts

--- src/utils/react-query.tsx
+++ src/utils/react-query.tsx
@@ -1,17 +1,20 @@
 "use client";
+
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from "react";
 
-const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+export default function ReactQueryProvider({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
-};
-
-export default ReactQueryProvider;
+  )
+}
