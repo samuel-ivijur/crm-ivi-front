3cfee1558f2c966b653aebcd975577fc539401bc
igorsantos1997 <igoropgl@gmail.com>
2025-01-19 23:58:21 -0300

    chore: config port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
     apps: [
       {
         name: "crm-ivi-front",
         script: "npm",
-        args: "start",
+        args: `start -- -p ${process.env.PORT || 3000}`,
         env: {
           NODE_ENV: "production",
         },
